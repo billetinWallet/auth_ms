8665222668fd3ea469be27cbbc384df4c1d1fe28
FelipeH22 <juan22herrera01@gmail.com>
2023-11-05 21:10:13 -0500

    Vm conn

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-db_url = 'postgresql://ms:gRIgBmN8KFuqmhcL2EV0vHmLDLojxinY@dpg-cko5idujmi5c73essu00-a/auth_db_7z9w'
-engine = create_engine(db_url)
+db_url = "postgresql://ms:gRIgBmN8KFuqmhcL2EV0vHmLDLojxinY@dpg-cko5idujmi5c73essu00-a.oregon-postgres.render.com/auth_db_7z9w"
+engine = create_engine(db_url, )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
